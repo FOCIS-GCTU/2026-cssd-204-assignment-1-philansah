239eddcb465ae8a0eae0c4928175e54f41565b3e
Philip Yaw Neequaye Ansah <2425403524@live.gctu.edu.gh>
2026-05-30 16:20:18 +0000

    completed the assignment

--- initials.py
+++ initials.py
@@ -1,7 +1,43 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number 1
+#
+# Name: Philip (Yaw) Neequaye Ansah 
+# STUDENT ID: 2425403524
+# Email: 2425403524@live.gctu.edu.gh
+#Grader: Augustus
+#
+# On my honor, Philip Yaw neequaye Ansah, this programming assignment is my own work
+# and I have not provided this code to any other student.
 
 def main():
-  pass #code goes here
+    print()
+    print("...PNA")
+    print()
+    
+    # Line 1
+    print("...PPPPPPPPPP.........NNNN....NNNN.........AAAAAA...")
+    # Line 2
+    print("...PPPPPPPPPPP........NNNNN...NNNN........AAAAAAAA..")
+    # Line 3
+    print("...PP.......PPP.......NN.NN...NNNN........AA....AA..")
+    # Line 4
+    print("...PP........PP.......NN..NN..NNNN........AA....AA..")
+    # Line 5
+    print("...PP.......PPP.......NN...NN.NNNN........AAAAAAAA..")
+    # Line 6
+    print("...PPPPPPPPPPP........NN....NNNNNN........AAAAAAAA..")
+    # Line 7
+    print("...PPPPPPPPPP.........NN.....NNNNN........AA....AA..")
+    # Line 8
+    print("...PP.................NN......NNNN........AA....AA..")
+    # Line 9
+    print("...PP............**...NN.......NNN...**...AA....AA..**")
+    # Line 10
+    print("...PP............**...NN........NN...**...AA....AA..**")
+    
+    print()
 
 
-
-main()
+if __name__ == "__main__":
+    main()
